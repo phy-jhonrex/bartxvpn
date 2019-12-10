504c504d0e270296cbc163b968c68ba569365f15
phy-jhonrex <58643365+phy-jhonrex@users.noreply.github.com>
2019-12-10 11:39:20 +0800

    Update bartxvpn.sh

--- bartxvpn.sh
+++ bartxvpn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Removing Old Theme"
-rm -rf /home/panel/html/asset
+rm -rf /home/panel/html/assets
 rm -rf /home/panel/html/view
 rm -rf /home/panel/html/tmp/*
 echo "Installing bartxvpn Themes"
